99626f324114619b99c8bda49b7f23f660f46fd7
Tanmay20030516 <tanmay.gawande.che21@itbhu.ac.in>
2023-09-24 22:59:39 +0530

    completed basic convolutional neural network

--- 05basic_neural_network.py
+++ 05basic_neural_network.py
@@ -13,6 +13,14 @@ from tqdm import tqdm  # progress bar customisation
 
 # 1. Creating our Neural network
 class DNN(nn.Module):  # subclassing from nn.Module
+    """
+    Here we define the layers of the network. We create two fully connected layers
+
+    Parameters:
+        input_size: the size of the input, in this case 784 (28x28)
+        num_classes: the number of classes we want to predict, in this case 10 (0-9)
+
+    """
     def __init__(self, input_size, num_classes):
         super(DNN, self).__init__()
         self.fc_layer1 = nn.Linear(input_size, 200)
@@ -21,6 +29,16 @@ class DNN(nn.Module):  # subclassing from nn.Module
         self.fc_layer4 = nn.Linear(50, num_classes)
 
     def forward(self, x):
+        """
+        x here is the mnist images, and we run it through fully connected layers that we created above.
+        we also add a ReLU activation function in between
+
+        Parameters:
+            x: mnist images
+
+        Returns:
+            out: the output of the network
+        """
         x = F.relu(self.fc_layer1(x))
         x = F.relu(self.fc_layer2(x))
         x = F.relu(self.fc_layer3(x))
@@ -36,7 +54,7 @@ INPUT_SIZE = 784  # 28*28
 NUM_CLASSES = 10
 LEARNING_RATE = 0.001
 BATCH_SIZE = 32
-NUM_EPOCHS = 10
+NUM_EPOCHS = 7
 
 # 4. Data Loading
 train_dataset = datasets.MNIST(
@@ -84,8 +102,22 @@ for epoch in range(NUM_EPOCHS):
 
 # 8. Accuracy measure
 def accuracy_score(loader, model):
+    """
+    Check accuracy of our trained model given a loader and a model
+
+    Parameters:
+        loader: torch.utils.data.DataLoader
+            A loader for the dataset you want to check accuracy on
+        model: nn.Module
+            The model you want to check accuracy on
+
+    Returns:
+        acc: float
+            The accuracy of the model on the dataset given by the loader
+    """
     num_correct = 0
     num_examples = 0
+    model.eval()  # evaluation mode
 
     with torch.no_grad():  # no need to track grads while testing
         for data, target in loader:
@@ -96,6 +128,7 @@ def accuracy_score(loader, model):
 
             # forward pass (for prediction)
             val = model(data)  # val.shape = (batch_size, predictions)
+            # so we want the index of maximum score
             _, predictions = val.max(dim=1)
 
             # assessing accuracy
