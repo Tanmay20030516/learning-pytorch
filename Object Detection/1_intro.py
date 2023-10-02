bf2bbe03713ea45dbdfa732e6f40cbf1bc0c9d62
Tanmay20030516 <tanmay.gawande.che21@itbhu.ac.in>
2023-10-02 19:54:42 +0530

    intro to object detection

---
+++ Object Detection/1_intro.py
@@ -0,0 +1,14 @@
+# Object localisation/detection via sliding window approach (sliding window approach is computationally very expensive)
+# OverFeat paper: https://arxiv.org/pdf/1312.6229.pdf
+
+# To overcome above approach problems
+# R-CNN paper: https://arxiv.org/pdf/1311.2524.pdf
+# Fast R-CNN paper: https://arxiv.org/pdf/1504.08083.pdf
+# Faster R-CNN paper: https://arxiv.org/pdf/1506.01497.pdf (use of RPNs)
+
+# To achieve near real-time object detection
+# YOLO (You Only Look Once) family (v1, v2, v3, v4)
+# YOLOv1 paper: https://arxiv.org/pdf/1506.02640.pdf
+# YOLOv2 paper: https://arxiv.org/pdf/1612.08242.pdf
+# YOLOv3 paper: https://arxiv.org/pdf/1804.02767.pdf
+# YOLOv4 paper: https://arxiv.org/pdf/2004.10934.pdf
